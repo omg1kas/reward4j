50974c10fad84e28b6168dad793c0a7c05f8ebe8
webmaster@reina-sofia.de <webmaster@reina-sofia.de@aac1df0a-b7e4-11de-a66b-f3ad5cb19abf>
2009-11-12 08:04:25 +0000

    Some domain objects.

---
+++ trunk/src/main/java/org/reward4j/model/Position.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2009 reward4j.org
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.reward4j.model;
+
+import java.util.Date;
+
+/**
+ * 
+ */
+public class Position {
+  /**
+   * Unique identifier.
+   */
+  private long id;
+
+  /**
+   * Date of insertion. Should always be sysdate and mandatory.
+   */
+  private Date idate = new Date();
+
+  /**
+   * Rate for this account position according to the underlying action.
+   */
+  private double value;
+  
+  /**
+   * 
+   */
+  private RateableAction action;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public RateableAction getAction() {
+    return action;
+  }
+
+  public void setAction(RateableAction action) {
+    this.action = action;
+  }
+
+  public Date getIdate() {
+    return idate;
+  }
+
+  public void setIdate(Date idate) {
+    this.idate = idate;
+  }
+}
---
+++ trunk/src/main/java/org/reward4j/model/User.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2009 reward4j.org
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.reward4j.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents a user in the reward4j domain.
+ */
+public class User {
+  private long id;
+  private String name;
+  private String pwd; // this property may not be needed - should be discussed
+  private List<Account> accounts = new ArrayList<Account>();
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getPwd() {
+    return pwd;
+  }
+
+  public void setPwd(String pwd) {
+    this.pwd = pwd;
+  }
+
+  public List<Account> getAccounts() {
+    return accounts;
+  }
+
+  public void setAccounts(List<Account> accounts) {
+    this.accounts = accounts;
+  }
+}
