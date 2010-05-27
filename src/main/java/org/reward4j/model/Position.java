d5c4ba74cbff7197b8ebcc3b0ad259ca56f540ae
hillger.t <hillger.t@googlemail.com>
2010-05-27 15:37:11 +0000

    Some jpa specific annotation added.

--- src/main/java/org/reward4j/model/Position.java
+++ src/main/java/org/reward4j/model/Position.java
@@ -20,6 +20,16 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.UUID;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.apache.commons.lang.builder.ToStringBuilder;
@@ -28,89 +38,97 @@ import org.apache.commons.lang.builder.ToStringBuilder;
  * The {@code Position} represents a position within a user's {@link Account}. After 
  * creation a {@code Position} is immutable.
  */
+@Entity
 public class Position implements Serializable {
-    private static final long serialVersionUID = -4937858211810042799L;
+	private static final long serialVersionUID = -4937858211810042799L;
 
-    // Unique identifier.
-    private long id;
+	// Unique identifier.
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
 
-    // Date of insertion. Should always be sysdate and mandatory.
-    private Date insertionDate = new Date();
+	// Date of insertion. Should always be sysdate and mandatory.
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date insertionDate = new Date();
 
-    // Rate for this account position according to the underlying action.
-    private Coin balance;
+	// Points to the position's {@link Account}.
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "accountid")
+	private Account account;
 
-    // The origin {@link RateableAction} that forces this position
-    private RateableAction action;
-    
-    public Position(RateableAction action, Coin balance) {
-        if(null==action) throw new IllegalArgumentException("action must not be null");
-        if(null==balance) throw new IllegalArgumentException("balance must not be null");
-        
-        this.id = UUID.randomUUID().getMostSignificantBits();
-        this.action = action;
-        this.balance = balance;
-    }
+	// Rate for this account position according to the underlying action.
+	private double balance;
 
-    public long getId() {
-        return id;
-    }
-    
-    protected void setId(long id) {
-        this.id = id;
-    }
+	// The origin {@link RateableAction} that forces this position
+	@OneToOne(targetEntity = RateableAction.class, optional = false)
+	@JoinColumn(name = "actionid")
+	private RateableAction action;
 
-    public RateableAction getAction() {
-        return action;
-    }
+	public Position(RateableAction action, Coin balance) {
+		if (null == action)
+			throw new IllegalArgumentException("action must not be null");
+		if (null == balance)
+			throw new IllegalArgumentException("balance must not be null");
 
-    public Date getInsertionDate() {
-        return (Date)insertionDate.clone();
-    }
+		this.id = UUID.randomUUID().getMostSignificantBits();
+		this.action = action;
+		this.balance = balance.amount();
+	}
 
-    public Coin getValue() {
-        return balance;
-    }
+	public long getId() {
+		return id;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        
-        if (obj == null) { 
-            return false; 
-        }
-        if (obj == this) { 
-            return true; 
-        }
-        if (obj.getClass() != getClass()) {
-            return false;
-        }
-        
-        Position rhs = (Position) obj;
-        
-        return new EqualsBuilder()
-            .append(this.id, rhs.id)
-            .append(this.action, rhs.action)
-            .append(this.balance, rhs.balance)
-            .isEquals();
-    }
+	protected void setId(long id) {
+		this.id = id;
+	}
 
-    @Override
-    public int hashCode() {
-        return new HashCodeBuilder()
-            .append(this.id)
-            .append(this.action)
-            .append(this.balance)
-            .toHashCode(); 
-    }
+	public RateableAction getAction() {
+		return action;
+	}
 
-    @Override
-    public String toString() {
-        return new ToStringBuilder(this)
-            .append("id", this.id)
-            .append("action", this.action)
-            .append("balance", this.balance)
-            .append("insertionDate", this.insertionDate)
-            .toString();
-    }
-    
+	public Date getInsertionDate() {
+		return (Date) insertionDate.clone();
+	}
+
+	public Coin getValue() {
+		return new Coin(this.balance);
+	}
+
+	public Account getAccount() {
+		return account;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+
+		if (obj == null) {
+			return false;
+		}
+		if (obj == this) {
+			return true;
+		}
+		if (obj.getClass() != getClass()) {
+			return false;
+		}
+
+		Position rhs = (Position) obj;
+
+		return new EqualsBuilder().append(this.id, rhs.id).append(this.action, rhs.action).append(this.balance, rhs.balance).isEquals();
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder().append(this.id).append(this.action).append(this.balance).toHashCode();
+	}
+
+	@Override
+	public String toString() {
+		return new ToStringBuilder(this).append("id", this.id).append("action", this.action).append("balance", this.balance).append("insertionDate", this.insertionDate).toString();
+	}
+
 }
